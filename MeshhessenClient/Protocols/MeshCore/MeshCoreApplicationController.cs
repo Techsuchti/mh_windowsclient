8a38d7d5f886e83d331471e5dcd52b578426fd81
Techsuchti <techsuchti@ik.me>
2026-08-22 19:09:56 +0200

    fix: align MeshCore node registry change event

--- MeshhessenClient/Protocols/MeshCore/MeshCoreApplicationController.cs
+++ MeshhessenClient/Protocols/MeshCore/MeshCoreApplicationController.cs
@@ -16,7 +16,7 @@ public sealed class MeshCoreApplicationController : IDisposable
     public event EventHandler<MeshCoreDeviceInfo>? DeviceChanged;
     public event EventHandler<MeshCoreSelfInfo>? SelfChanged;
     public event EventHandler<IReadOnlyList<MeshCoreContact>>? ContactsChanged;
-    public event EventHandler<IReadOnlyList<MeshCoreNode>>? NodesChanged;
+    public event EventHandler? NodesChanged;
     public event EventHandler<MeshCoreChannel>? ChannelReceived;
     public event EventHandler<MeshCoreMessage>? MessageReceived;
     public event EventHandler<byte[]>? PushReceived;
@@ -56,7 +56,7 @@ public sealed class MeshCoreApplicationController : IDisposable
     {
         NodeRegistry.UpsertRange(contacts);
         ContactsChanged?.Invoke(this, contacts);
-        NodesChanged?.Invoke(this, NodeRegistry.Snapshot());
+        NodesChanged?.Invoke(this, EventArgs.Empty);
     }
 
     public void Dispose()
